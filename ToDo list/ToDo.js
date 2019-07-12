68511030764caa195ba2b2cfa347aba87f1495f6
reddevilcero <reddevil_cero@hotmail.com>
2019-07-12 09:28:40 +0100

    minor css improvements and on click functionality added

--- ToDo list/ToDo.js
+++ ToDo list/ToDo.js
@@ -9,9 +9,26 @@ $('span').on('click',function () {
     }
     
 });
+
 $("input[type='text'").on('keypress', function (e) {
     if (e.which === 13) {
         $('ul').append(`<li>${input.val()}</li>`)
         input.val('')
     } 
+});
+
+$('ul').on('click', 'li', function (e) {
+  /*e.offsetX return the x coordinate of the click event and we check if is done in
+     the after element set at 40 px because we move in css -40px and in hover we return
+     it at its natural place.*/
+    if (e.offsetX > 40) { 
+       $(this).addClass('done')
+    }
+     else{
+       // click on :before element
+      $(this).slideUp(500, function () {
+          $(this).remove();
+      })
+
+   }
 });
