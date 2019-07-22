f8c7318b2b1e2ba991505783b4fa2ec7066d20a8
reddevilcero <reddevil_cero@hotmail.com>
2019-07-22 13:45:29 +0100

    basic boostrap 4 styling

--- YelpCamp/v1/index.js
+++ YelpCamp/v1/index.js
@@ -9,6 +9,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 var campsGrounds = [
     {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
     {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
+    {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
+    {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
+    {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
+    {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
+    {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
+    {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
     {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},
 ]
 
