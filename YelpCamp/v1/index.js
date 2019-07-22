d4254e492a8d1d302cbb8337a0118ea7021990e1
reddevilcero <reddevil_cero@hotmail.com>
2019-07-22 15:35:48 +0100

    navbar added and morre styles and some spelling fixed.

--- YelpCamp/v1/index.js
+++ YelpCamp/v1/index.js
@@ -6,7 +6,7 @@ app.set('view engine','ejs');
 app.use(express.static('statics'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var campsGrounds = [
+var campgrounds = [
     {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
     {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
     {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
@@ -22,23 +22,23 @@ app.get('/', (req, res)=>{
     res.render('home')
 })
 
-app.get('/campsgrounds', (req, res)=>{
+app.get('/campgrounds', (req, res)=>{
   
 
-    res.render('campsgrounds',{listOfCampsGrounds:campsGrounds});
+    res.render('campgrounds',{listOfCampgrounds:campgrounds});
 });
 
-app.post('/campsgrounds',(req, res)=>{
+app.post('/campgrounds',(req, res)=>{
     let newCamp = {
         name:req.body.name,
         img: req.body.image,
     }
     console.log(req.body)
-    campsGrounds.push(newCamp);
-    res.redirect('/campsgrounds')
+    campgrounds.push(newCamp);
+    res.redirect('/campgrounds')
 })
 
-app.get('/campsgrounds/new', (req, res)=>{
+app.get('/campgrounds/new', (req, res)=>{
     res.render('new')
 });
 
