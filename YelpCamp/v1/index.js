1a7344ac328f40a464b5b845de50bd24d5a88bb5
reddevilcero <reddevil_cero@hotmail.com>
2019-07-23 12:01:24 +0100

    new mongoose conection, create and find campgrounds.

--- YelpCamp/v1/index.js
+++ YelpCamp/v1/index.js
@@ -1,41 +1,69 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/YelpCamp', { useNewUrlParser: true }).
+  catch(error => handleError(error));
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to MongoDB')
+ 
+});
+
+var campgroundSchema = new mongoose.Schema({
+    name:String,
+    imgUrl:String,
+    });
+
+    var Campground = mongoose.model('Campground', campgroundSchema);
+
+    // Campground.create(
+    //     {name:'Salmon creek', imgUrl:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
+    //     (err, campground)=>{
+    //     if (err) {
+    //         console.log(err)
+    //     } else {
+    //         console.log(`New Campground ${campground.name} Created`)
+    //     }
+    // });
 
 app.set('view engine','ejs');
 app.use(express.static('statics'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var campgrounds = [
-    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
-    {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
-    {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
-    {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
-    {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
-    {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
-    {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},    {name:'Salmon creek', img:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
-    {name:'Granite Hill', img:'https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg'},
-    {name:'Villa Rusu', img:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg'},
-]
 
 app.get('/', (req, res)=>{
     res.render('home')
 })
 
 app.get('/campgrounds', (req, res)=>{
-  
-
-    res.render('campgrounds',{listOfCampgrounds:campgrounds});
+    Campground.find({}, (err, campgrounds)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds',{listOfCampgrounds:campgrounds});
+        }
+    })
+    
 });
 
 app.post('/campgrounds',(req, res)=>{
     let newCamp = {
         name:req.body.name,
-        img: req.body.image,
+        imgUrl: req.body.image,
     }
-    console.log(req.body)
-    campgrounds.push(newCamp);
-    res.redirect('/campgrounds')
+    Campground.create(newCamp,err=>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds')
+        }
+        
+    })
+    
 })
 
 app.get('/campgrounds/new', (req, res)=>{
