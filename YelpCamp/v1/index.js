b72ebe9c9b02e93114d9c24c2b3514da3130241c
reddevilcero <reddevil_cero@hotmail.com>
2019-07-26 10:59:46 +0100

    refactor adding models require and module

--- YelpCamp/v1/index.js
+++ YelpCamp/v1/index.js
@@ -1,7 +1,8 @@
 const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
-      mongoose = require('mongoose')
+      mongoose = require('mongoose'),
+      Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/YelpCamp', { useNewUrlParser: true }).
   catch(error => handleError(error));
@@ -12,24 +13,6 @@ db.once('open', function() {
   console.log('connected to MongoDB')
  
 });
-
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    imgUrl: String,
-    description: String,
-    });
-
-    var Campground = mongoose.model('Campground', campgroundSchema);
-
-    // Campground.create(
-    //     {name:'Salmon creek', imgUrl:'https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg'},
-    //     (err, campground)=>{
-    //     if (err) {
-    //         console.log(err)
-    //     } else {
-    //         console.log(`New Campground ${campground.name} Created`)
-    //     }
-    // });
 
 app.set('view engine','ejs');
 app.use(express.static('statics'));
